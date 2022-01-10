44f9f08480102f1205d73539ddc4d2bda02674ca
Avik S <eml2avik@gmail.com>
2022-01-11 00:32:26 +0530

    controllers added

--- proj/auth/controller/__init__.py
+++ proj/auth/controller/__init__.py
@@ -5,10 +5,42 @@ except ImportError:
 
 from proj.app.base.api.main import Resource
 from proj.app import settings
+from proj.app.dbops import Location, Department, Category
+from sqlalchemy import and_
 
 
-class LoginView(Resource):
+class GetLocations(Resource):
 
     def get(self, *args, **kwargs):
-        return {}
-    
+        with settings.Session() as session:
+            res = session.query(Location).with_entities(Location.id, Location.name)
+            res = [{'id': r[0], 'name': r[1]} for r in res]
+        return {'data': res}
+
+
+class GetDepartments(Resource):
+    def get(self, *args, **kwargs):
+        try:
+            location_id = int(kwargs['location_id'])
+        except ValueError:
+            return {'data': []}
+        with settings.Session() as session:
+            res = session.query(Department).filter(Department.location_id == location_id).with_entities(
+                Department.id, Department.name)
+            res = [{'id': r[0], 'name': r[1]} for r in res]
+        return {'data': res}
+
+
+class GetCategories(Resource):
+    def get(self, *args, **kwargs):
+        try:
+            location_id = int(kwargs['location_id'])
+            department_id = int(kwargs['department_id'])
+        except ValueError:
+            return {'data': []}
+        with settings.Session() as session:
+            res = session.query(Category).join(Department).filter(
+                and_(Department.id == department_id, Department.location_id == location_id)
+            ).with_entities(Category.id, Category.name)
+            res = [{'id': r[0], 'name': r[1]} for r in res]
+        return {'data': res}
--- proj/auth/routes.py
+++ proj/auth/routes.py
@@ -1,3 +1,5 @@
 routes = [
-    ('loginview', '/login'),
-    ]
+    ('getlocations', '/api/v1/location'),
+    ('getdepartments', '/api/v1/location/{location_id}/department'),
+    ('getcategories', '/api/v1/location/{location_id}/department/{department_id}/category')
+]
